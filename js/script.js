7ad17c7c032ec77753e158fafb613e9a9c442e38
Altemir Almeida <altemir.git@gmail.com>
2020-04-05 18:30:45 +0100

    Add active button and js for profile info

--- js/script.js
+++ js/script.js
@@ -3,12 +3,35 @@ const profile = document.querySelector('.profile');
 const profileObj = {
     name: 'Altemir Almeida',
     email: 'altemirap25@gmail.com',
-    summary: 'Hi, my name is Altemir, asdjfaskldjfçaskljdfçalksdjfçalksdjfçaslkdjf '
+    summaryp1: `Hi, My name is Altemir Almeida. I've been living in Ireland for almost 3 years. 
+    I decided to move to Ireland to learn english, 
+    and since last 17<sup>th</sup> february, I started a postgraduation in Science in Computing at CCT.
+    But back in Brazil I graduated in Information Systems at Estácio.`,
+    summaryp2: `During my graduation I worked as Web Mobile developer for government agencies,
+    developing systems for mobile (Android and IOS) and Web, using technologies as Jquery, Jquery Mobile, 
+    HTML, CSS, JavaScript and PhoneGap.`
+
 }
 
 let addProfile = () => {
     var template = `
+            <ul class="profile-data">
+                <li>${profileObj.name}</li>
+                <li>${profileObj.email}</li>
+            </ul>
+            <div class="icons">
+                <a href="https://www.linkedin.com/in/altemir-almeida-87868872/" target="_blank"><img src="images/in.png" class="icon circle"></a>
+                <a href="https://github.com/Altemir-Ap/" target="_blank"><img src="images/git.png" class="icon"></a>
+            </div>
+            <span class="summary skill-summary container shadow container column">
+                <p> 
+                &nbsp;&nbsp;${profileObj.summaryp1}
 
+                </p>
+                <p> 
+                &nbsp; &nbsp${profileObj.summaryp2};
+                </p>
+            </span>
     `;
     profile.insertAdjacentHTML("beforeend", template)
 }
